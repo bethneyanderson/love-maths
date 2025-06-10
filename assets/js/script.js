163d1ecc0fc45abab12fd13ab860272a5b784fbe
bethneyanderson <bethneyanderson95@gmail.com>
2025-06-10 14:46:30 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswers() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
